c1ed618e9ff458c46015d41ffed70fe8d926ddcf
user <hunchan12@gmail.com>
2018-10-25 09:49:33 +0900

    내사이트 투표결과

--- polls/urls.py
+++ polls/urls.py
@@ -2,11 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+# / 붙이면 현재나의 경로는 vote까지, 없으면 그 전까지 현재경로 , /가 어디서 마무리 됐는가가 경로가 된다.
+#  "/" : 상대 경로에서 현재 나의 경로의 기준 이 됨
 urlpatterns = [
+    path('result/<int:id>', views.result),
     path('input', views.input),
     path('add_question', views.add_question),
-    path('data', views.data),
+    path('<int:number>/data/<str:email>', views.data),
     path('', views.index),
     path('<int:id>', views.detail, name='detail'),
     path('vote/', views.vote, name='vote'),
--- polls/views.py
+++ polls/views.py
@@ -9,17 +9,18 @@ def input(request):
 
 def add_question(request):
     text = request.POST['text']
-    q = Question(
-        question_text=text,
-        pub_date=timezone.now())
-    q.save()
+    # q = Question(
+    #     question_text=text,
+    #     pub_date=timezone.now())
+    # q.save()
     # return render(request, 'polls/.html', {})
     return HttpResponse('입력완료')
 
 
-def data(request):
+def data(request, email, number):
     value = request.GET['user_name']
-    return HttpResponse
+    return HttpResponse(
+        value + email + str(number))
 
 def vote(request):
     choice = request.POST['choice']
@@ -39,3 +40,8 @@ def index(request):
 
     list = Question.objects.all()
     return render(request, 'polls/index.html', {'question': list})
+
+def result(request, id):
+    question = Question.objects.get(pk=id)
+    return render(request, 'polls/result.html',
+                  {'question': question})
