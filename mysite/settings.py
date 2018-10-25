98d36024478ce26188b4cc32ca6fe5fdd02b154e
user <hunchan12@gmail.com>
2018-10-25 11:05:53 +0900

    내사이트 투표결과3

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wqa(jb^a2)r5@57gf*&8tl1spt!llpesfcp1dh88ijum*jv^%1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
